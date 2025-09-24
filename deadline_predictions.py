78b62326cdc104a1219676be25782739b33ed15f
bnolinn <137727884+bnolinn@users.noreply.github.com>
2025-09-24 13:01:15 -0700

    Delete deadline_predictions.py

--- deadline_predictions.py
+++
@@ -1,8 +0,0 @@
-from pybaseball import standings, team_pitching
-from sklearn.ensemble import RandomForestRegressor
-
-df_standings = standings(2025)
-print(df_standings)
-
-df_pitching = team_pitching(2025)
-print(df_pitching)
